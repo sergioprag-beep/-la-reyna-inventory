50844e4dadfa918320e2c9debef7feedf7d18867
sergioprag-beep <sergioprag@gmail.com>
2026-09-03 15:58:39 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='lrx-stable-v5';self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>r).catch(()=>caches.match(e.request).then(x=>x||caches.match('./offline.html'))))});
