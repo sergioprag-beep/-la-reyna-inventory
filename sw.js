189b5576283c4265ad439be49d5f3d7e8450c2ac
sergioprag-beep <sergioprag@gmail.com>
2026-09-07 23:07:29 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lrx-v58-4-pos-movements';
+const CACHE='lrx-v58-5-dashboard-search';
 const ASSETS=['./','./index.html','./offline.html'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
