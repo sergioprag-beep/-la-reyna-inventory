e7a49323789ba2d7080738b24068a97ad4d44b02
sergioprag-beep <sergioprag@gmail.com>
2026-09-13 15:22:46 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,5 @@
-const CACHE='lrx-pro-v66-inventory-save-fix';
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET') return;
-  e.respondWith(fetch(e.request).then(r=>{
-    const c=r.clone(); caches.open(CACHE).then(x=>x.put(e.request,c)); return r;
-  }).catch(()=>caches.match(e.request)));
-});
+const CACHE='lrx-pro-v67-pos-add-fix';
+const ASSETS=['./','./index.html','./manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html')))));
