eeb3f6435f0f9079502edef81b73417daf4d4523
sergioprag-beep <sergioprag@gmail.com>
2026-09-12 21:53:00 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,9 @@
-const CACHE='lrx-pro-v65-inventory-actions-fix';
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.add('./')).then(()=>self.skipWaiting()));
-});
-self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
-});
+const CACHE='lrx-pro-v66-inventory-save-fix';
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET')return;
+  if(e.request.method!=='GET') return;
   e.respondWith(fetch(e.request).then(r=>{
-    const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;
-  }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./'))));
+    const c=r.clone(); caches.open(CACHE).then(x=>x.put(e.request,c)); return r;
+  }).catch(()=>caches.match(e.request)));
 });
