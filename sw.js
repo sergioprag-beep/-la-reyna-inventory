0fa6fbadbc3baba7f71915d753bfed511f95c53a
sergioprag-beep <sergioprag@gmail.com>
2026-09-14 22:12:02 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,7 @@
-const CACHE='lrx-v107.2';
-const CORE=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE).catch(()=>{})));});
-self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+const CACHE='lrx-v107.3';
+self.addEventListener('install',event=>{self.skipWaiting()});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET') return;
-  const url=new URL(event.request.url);
-  if(url.origin!==location.origin) return;
-  // Always prefer the network so GitHub Pages updates are visible immediately.
-  event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>caches.match(event.request)));
+  event.respondWith(fetch(event.request,{cache:'no-store'}).then(r=>{const copy=r.clone(); if(new URL(event.request.url).origin===self.location.origin) caches.open(CACHE).then(c=>c.put(event.request,copy)).catch(()=>{}); return r}).catch(()=>caches.match(event.request)));
 });
