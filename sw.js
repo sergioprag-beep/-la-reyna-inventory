093ad067116bbb72bd5c0d8395e806df7c66df47
sergioprag-beep <sergioprag@gmail.com>
2026-09-15 15:01:12 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE='lrx-v115-clean';self.addEventListener('install',e=>e.waitUntil(self.skipWaiting()));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>{});
