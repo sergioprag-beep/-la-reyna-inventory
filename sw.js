94f4b20de5ceceeb24efafdc68cb64c51c020e2c
sergioprag-beep <sergioprag@gmail.com>
2026-09-16 19:37:42 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,45 +1,4 @@
-const CACHE='lrx-v133-pwa';
-const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
-
-self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));
-});
-
-self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-      .then(()=>self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch',event=>{
-  const req=event.request;
-  if(req.method!=='GET') return;
-
-  // Always try the network first for HTML navigations so installed copies
-  // do not remain stuck on an older application version.
-  if(req.mode==='navigate'){
-    event.respondWith(
-      fetch(req,{cache:'no-store'})
-        .then(res=>{
-          const copy=res.clone();
-          caches.open(CACHE).then(c=>c.put('./index.html',copy)).catch(()=>{});
-          return res;
-        })
-        .catch(()=>caches.match('./index.html'))
-    );
-    return;
-  }
-
-  // Static assets: cache first, then network.
-  event.respondWith(
-    caches.match(req).then(cached=>cached || fetch(req).then(res=>{
-      if(res && res.ok){
-        const copy=res.clone();
-        caches.open(CACHE).then(c=>c.put(req,copy)).catch(()=>{});
-      }
-      return res;
-    }))
-  );
-});
+const CACHE='lrx-v131-recipes-sup-stable';
+self.addEventListener('install',e=>e.waitUntil(self.skipWaiting()));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{});
