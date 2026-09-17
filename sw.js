0fd3f0eded1397d485b83546c4ca67e93c8ea66f
sergioprag-beep <sergioprag@gmail.com>
2026-09-16 23:03:42 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lrx-v138-buttons-fixed';
+const CACHE='lrx-v139-boot-fixed';
 self.addEventListener('install',e=>e.waitUntil(self.skipWaiting()));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{});
