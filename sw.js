35024c81ed067ef4270bac661583e0c7c5b03b95
sergioprag-beep <sergioprag@gmail.com>
2026-09-16 22:03:41 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lrx-v135-stable';
+const CACHE='lrx-v136-stable-core';
 self.addEventListener('install',e=>e.waitUntil(self.skipWaiting()));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{});
